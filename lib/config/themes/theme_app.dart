b065ac54c90f825443790a5b5efaa0a53bd95f1a
Gustavo Canul <gusssy12@gmail.com>
2025-05-17 00:37:08 -0600

    refactor AppTheme

--- lib/config/themes/theme_app.dart
+++ lib/config/themes/theme_app.dart
@@ -16,13 +16,30 @@ class AppTheme {
   assert(selectedColor >= 0, 'selectedColor must be greater than 0'),
   assert(selectedColor <= colorList.length -1, 'selectedColor must be less or equal than ${colorList.length - 1}' );
 
-  ThemeData getTheme() => ThemeData(
-    //*segun el modo oscuro o claro se cambia el brillo para los componentes de material
-    brightness: isDarkMode ? Brightness.dark : Brightness.light ,
-    useMaterial3: true,
-    //*este color se usa solo para material components, como por ejemplo: BottomNavigationBar
-    colorSchemeSeed: colorList[selectedColor],
-  );
+    ThemeData getTheme() {
+    final primary = colorList[selectedColor];
+    final secondary = Color(0xFFFFC300); // Ejemplo de color secundario
+    final surface = Colors.white;
+    final error = Colors.red;
+
+    final colorScheme = ColorScheme(
+      brightness: isDarkMode ? Brightness.dark : Brightness.light, //Define si la paleta de colores es clara u oscura
+      primary: primary, //El color pirncipal de la app - en componentes de material.
+      onPrimary: Colors.white, // Color de texto sobre el color primario - asegura tener contraste y legibilidad sobre el color principal.
+      secondary: secondary, // Color secundario de la app - resaltar elementos menos importantes que el color primario.
+      onSecondary: Colors.black, // Color de texto sobre el color secundario.
+      error: error, // Color de error - para mostrar errores o advertencias.
+      onError: Colors.white, // Color de texto sobre el color de error.
+      surface: surface,  // Color de superficie - para fondos de componentes como tarjetas o listas.
+      onSurface: Colors.black, // Color de texto sobre la superficie.
+    );
+
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: colorScheme,
+      // Puedes personalizar más propiedades aquí si lo deseas
+    );
+  }
 
   AppTheme copyWith({
     bool? isDarkMode,
